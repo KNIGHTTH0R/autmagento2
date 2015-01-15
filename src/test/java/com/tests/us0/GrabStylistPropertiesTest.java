f1b4e2214f3c87e5e49b58295f870764e68942c3
voicu.vac <voicu.vac@Evozon.com>
2015-01-15 13:54:00 +0200

    Updated demo test

--- src/test/java/com/tests/us0/GrabStylistPropertiesTest.java
+++ src/test/java/com/tests/us0/GrabStylistPropertiesTest.java
@@ -1,10 +1,8 @@
 package com.tests.us0;
 
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Properties;
 
 import net.thucydides.core.annotations.Steps;
