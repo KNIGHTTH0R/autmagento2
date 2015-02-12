222756eba58506a9958ffd9ac1a3024077d53bc8
voicu.vac <voicu.vac@Evozon.com>
2015-02-12 14:03:25 +0200

    Updated test suites

--- src/test/java/com/suites/US002Suite.java
+++ src/test/java/com/suites/US002Suite.java
@@ -4,14 +4,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-
 import com.tests.us2.US002CartSegmentationLogicTest;
+import com.tests.us2.US002UserProfileOrderIdTest;
 import com.tests.us2.US002ValidateOrderBackOfficeTest;
+import com.tests.us2.US002ValidateOrderEmailTest;
 
 
 @SuiteClasses({
 	US002CartSegmentationLogicTest.class,
+	US002UserProfileOrderIdTest.class,
 	US002ValidateOrderBackOfficeTest.class,
+	US002ValidateOrderEmailTest.class,
 })
 @RunWith(Suite.class)
 public class US002Suite {
--- src/test/java/com/suites/US003Suite.java
+++ src/test/java/com/suites/US003Suite.java
@@ -5,11 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.tests.us3.US003CartSegmentationWithVatTest;
+import com.tests.us3.US003UserProfileOrderIdTest;
 import com.tests.us3.US003ValidateOrderBackOfficeTest;
 import com.tests.us3.US003ValidateOrderEmailTest;
 
 @SuiteClasses({
 	US003CartSegmentationWithVatTest.class,
+	US003UserProfileOrderIdTest.class,
 	US003ValidateOrderEmailTest.class,
 	US003ValidateOrderBackOfficeTest.class,
 })
