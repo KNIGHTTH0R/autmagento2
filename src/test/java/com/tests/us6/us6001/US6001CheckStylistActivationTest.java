6dfd2b267d1061639c7b605ca1859fb5b7d50ec9
voicu.vac <voicu.vac@Evozon.com>
2015-03-12 14:16:23 +0200

    Updated test structure

---
+++ src/test/java/com/tests/us6/us6001/US6001CheckStylistActivationTest.java
@@ -0,0 +1,125 @@
+package com.tests.us6.us6001;
+
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.steps.backend.BackEndSteps;
+import com.steps.backend.OrdersSteps;
+import com.steps.backend.validations.StylistValidationSteps;
+import com.steps.external.EmailClientSteps;
+import com.tests.BaseTest;
+import com.tools.Constants;
+import com.tools.CustomVerification;
+import com.tools.data.backend.CustomerConfigurationModel;
+import com.tools.data.backend.StylistPropertiesModel;
+import com.tools.data.backend.StylistRegistrationAndActivationDateModel;
+import com.tools.persistance.MongoReader;
+import com.tools.requirements.Application;
+import com.workflows.backend.CustomerAndStylistRegistrationWorkflows;
+
+@WithTag(name = "US6001", type = "external,backend")
+@Story(Application.Stylist.CreateColaborator.class)
+@RunWith(ThucydidesRunner.class)
+public class US6001CheckStylistActivationTest extends BaseTest {
+
+	@Steps
+	public BackEndSteps backEndSteps;
+	@Steps	
+	public OrdersSteps ordersSteps;
+	@Steps
+	public EmailClientSteps emailClientSteps;
+	@Steps 
+	public CustomVerification customVerifications;
+	@Steps 
+	public StylistValidationSteps stylistValidationSteps;
+	@Steps 
+	public CustomerAndStylistRegistrationWorkflows customerAndStylistRegistrationWorkflows;
+
+	public CustomerConfigurationModel customerConfigurationModel = new CustomerConfigurationModel();
+	
+	public StylistPropertiesModel beforeLinkConfirmationStylistExpectedProperties = new StylistPropertiesModel();
+	public StylistPropertiesModel afterLinkConfirmationStylistExpectedProperties = new StylistPropertiesModel();
+	public StylistPropertiesModel afterOrderPaidStylistExpectedProperties = new StylistPropertiesModel();
+	public StylistRegistrationAndActivationDateModel datesModel = new StylistRegistrationAndActivationDateModel();
+
+	public String customerType;
+	public String stylistFirstName;
+	public String stylistEmail;
+	public String confirmationEmail;
+	public String validateAccount;
+	public String jewelryValue;
+	public String date;
+	public String email2;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		date = MongoReader.grabStylistDateModels("US6001StyleCoachRegistrationTest").get(0).getDate();
+		int size = MongoReader.grabStylistFormModels("US6001StyleCoachRegistrationTest").size();
+		if (size > 0) {
+			stylistEmail = MongoReader.grabStylistFormModels("US6001StyleCoachRegistrationTest").get(0).getEmailName();
+			stylistFirstName = MongoReader.grabStylistFormModels("US6001StyleCoachRegistrationTest").get(0).getFirstName();
+			System.out.println(stylistEmail);
+		} else
+			System.out.println("The database has no entries");
+
+		beforeLinkConfirmationStylistExpectedProperties =  new StylistPropertiesModel(Constants.NOT_CONFIRMED, Constants.JEWELRY_INITIAL_VALUE, Constants.GENERAL);
+		afterLinkConfirmationStylistExpectedProperties =  new StylistPropertiesModel(Constants.CONFIRMED, Constants.JEWELRY_INITIAL_VALUE, Constants.GENERAL);
+		afterOrderPaidStylistExpectedProperties =  new StylistPropertiesModel(Constants.CONFIRMED, Constants.JEWELRY_FINAL_VALUE, Constants.STYLIST);
+		datesModel = new StylistRegistrationAndActivationDateModel(date,date);
+		
+	}
+
+
+	@Test
+	public void us000CheckCustomerActivationTest() {
+
+		
+		backEndSteps.performAdminLogin(Constants.BE_USER, Constants.BE_PASS);
+		backEndSteps.clickOnCustomers();
+		backEndSteps.searchForEmail(stylistEmail);
+		String customerURL = backEndSteps.openCustomerDetails(stylistEmail);
+		
+		StylistPropertiesModel beforeLinkConfirmationStylistProperties =  backEndSteps.grabCustomerConfiguration();
+		
+		emailClientSteps.openMailinator();
+		confirmationEmail  = emailClientSteps.grabEmail(stylistEmail.replace("@" + Constants.WEB_MAIL, ""),"Benutzerkonto");
+		
+		backEndSteps.navigate(customerURL);
+		StylistPropertiesModel afterLinkConfirmationStylistProperties =  backEndSteps.grabCustomerConfiguration();
+
+		backEndSteps.clickOnSalesOrders();
+		backEndSteps.searchOrderByName(stylistFirstName);
+		backEndSteps.openOrderDetails(stylistFirstName);		
+		ordersSteps.markOrderAsPaid();	
+		
+		backEndSteps.clickOnCustomers();
+		backEndSteps.searchForEmail(stylistEmail);
+		backEndSteps.openCustomerDetails(stylistEmail);
+		
+		StylistPropertiesModel afterOrderPaidStylistProperties =  backEndSteps.grabCustomerConfiguration();	
+		StylistRegistrationAndActivationDateModel grabbeddatesModel = backEndSteps.grabStylistRegistrationAndConfirmationDates();
+
+		customerAndStylistRegistrationWorkflows.setValidateStylistProperties(beforeLinkConfirmationStylistProperties, beforeLinkConfirmationStylistExpectedProperties);	
+		customerAndStylistRegistrationWorkflows.validateStylistProperties("BEFORE CONFIRMATION LINK");
+		
+		customerAndStylistRegistrationWorkflows.setValidateStylistProperties(afterLinkConfirmationStylistProperties, afterLinkConfirmationStylistExpectedProperties);
+		customerAndStylistRegistrationWorkflows.validateStylistProperties("AFTER CONFIRMATION LINK");
+		
+		customerAndStylistRegistrationWorkflows.setValidateStylistProperties(afterOrderPaidStylistProperties, afterOrderPaidStylistExpectedProperties);
+		customerAndStylistRegistrationWorkflows.validateStylistProperties("AFTER MARK AS PAID ");
+		
+		customerAndStylistRegistrationWorkflows.setValidateStylistDates(grabbeddatesModel,datesModel);
+		customerAndStylistRegistrationWorkflows.validateStylistDAtes("VALIDATE REGISTRATION AND ACTIVATION DATES");
+		
+		customVerifications.printErrors();
+	}
+
+
+}
---
+++ src/test/java/com/tests/us6/us6001/US6001StyleCoachRegistrationTest.java
@@ -0,0 +1,74 @@
+package com.tests.us6.us6001;
+
+import java.io.IOException;
+
+import javax.xml.soap.SOAPException;
+
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.annotations.Story;
+import net.thucydides.core.annotations.WithTag;
+import net.thucydides.junit.runners.ThucydidesRunner;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.connectors.mongo.MongoConnector;
+import com.steps.frontend.HeaderSteps;
+import com.steps.frontend.StylistCampaignSteps;
+import com.steps.frontend.StylistRegistrationSteps;
+import com.tests.BaseTest;
+import com.tools.CustomVerification;
+import com.tools.data.StylistDataModel;
+import com.tools.data.frontend.AddressModel;
+import com.tools.data.frontend.CustomerFormModel;
+import com.tools.data.frontend.DateModel;
+import com.tools.persistance.MongoWriter;
+import com.tools.requirements.Application;
+
+
+@WithTag(name = "US6001", type = "frontend")
+@Story(Application.StyleCoach.Shopping.class)
+@RunWith(ThucydidesRunner.class)
+public class US6001StyleCoachRegistrationTest extends BaseTest{
+	
+	@Steps
+	public HeaderSteps headerSteps;
+	
+	@Steps
+	public StylistCampaignSteps stylistCampaignSteps;
+	
+	@Steps
+	public StylistRegistrationSteps stylistRegistrationSteps;
+	@Steps 
+	public CustomVerification customVerifications;
+
+	public CustomerFormModel dataModel;
+	public DateModel dateModel = new DateModel();
+	public AddressModel addressModel;
+	public StylistDataModel validationModel;
+
+	@Before
+	public void setUp() throws Exception {
+		// Generate data for this test run
+		dataModel = new CustomerFormModel();
+		addressModel = new AddressModel();
+		MongoConnector.cleanCollection(getClass().getSimpleName());
+	}
+	
+	@Test
+	public void us6001StyleCoachRegistrationTest() throws SOAPException, IOException{
+		headerSteps.navigateToRegisterform();
+		String date = stylistRegistrationSteps.fillCreateCustomerForm(dataModel, addressModel);
+		dateModel.setDate(date);
+		customVerifications.printErrors();		
+	}
+	
+	@After
+	public void saveData() {
+		MongoWriter.saveStylistFormModel(dataModel, getClass().getSimpleName());
+		MongoWriter.saveDateModel(dateModel, getClass().getSimpleName());
+		
+	}
+}
