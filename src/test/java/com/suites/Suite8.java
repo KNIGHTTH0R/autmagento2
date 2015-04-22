e2a2c9340b41a2e875bf70492f86c446fd7f5650
voicu.vac <voicu.vac@Evozon.com>
2015-04-22 15:15:48 +0300

    Created Suite 8 for running all subSuites

---
+++ src/test/java/com/suites/Suite8.java
@@ -0,0 +1,16 @@
+package com.suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@SuiteClasses({
+	US8001Suite.class,
+	US8002Suite.class,
+	US8003Suite.class,
+	
+})
+@RunWith(Suite.class)
+public class Suite8 {
+
+}
