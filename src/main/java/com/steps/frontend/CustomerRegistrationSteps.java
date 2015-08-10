143bd9676119a77ecceadb9d67ca9d0ab994444f
mihaibarta <mihai.barta@evozon.com>
2015-08-10 08:39:13 +0300

    minor fix

--- src/main/java/com/steps/frontend/CustomerRegistrationSteps.java
+++ src/main/java/com/steps/frontend/CustomerRegistrationSteps.java
@@ -526,7 +526,7 @@ public class CustomerRegistrationSteps extends AbstractSteps {
 			waitABit(2000);
 			pageTitle = thankYouPage().pageTitle();
 			counter++;
-		} while (!pageTitle.contains("Thank you page") && counter < 30);
+		} while (!pageTitle.contains("Thank you page") && counter < 45);
 		System.out.println(pageTitle);
 		Assert.assertTrue("Failure: Page title is not as expected. Might be a wrong page. Actual: " + pageTitle, pageTitle.contains("PIPPA&JEAN"));
 
