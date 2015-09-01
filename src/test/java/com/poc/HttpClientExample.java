07bd983d15a287795b8440beca1b16cf9af37875
mihaibarta <mihai.barta@evozon.com>
2015-09-01 10:30:28 +0300

    minor fix

--- src/test/java/com/poc/HttpClientExample.java
+++ src/test/java/com/poc/HttpClientExample.java
@@ -27,30 +27,30 @@ public class HttpClientExample {
 
 	}
 
-//	public String sendGet(String url) throws ClientProtocolException, IOException {
-//
-//		CloseableHttpClient httpclient = HttpClients.createDefault();
-//		HttpGet httpGet = new HttpGet(url);
-//		CloseableHttpResponse response1 = httpclient.execute(httpGet);
-//		String unparsedResponse;
-//
-//		try {
-//			System.out.println(response1.getStatusLine());
-//			HttpEntity entity = response1.getEntity();
-//			unparsedResponse = EntityUtils.toString(entity);
-//			EntityUtils.consume(entity);
-//		} finally {
-//			response1.close();
-//		}
-//		return unparsedResponse;
-//
-//	}
-//
-//	public void setAuthentication(String host, int port, String username, String password) {
-//		CredentialsProvider credsProvider = new BasicCredentialsProvider();
-//		credsProvider.setCredentials(new AuthScope(host, port), new UsernamePasswordCredentials(username, password));
-//		CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
-//	}
+	public String sendGet(String url) throws ClientProtocolException, IOException {
+
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		HttpGet httpGet = new HttpGet(url);
+		CloseableHttpResponse response1 = httpclient.execute(httpGet);
+		String unparsedResponse;
+
+		try {
+			System.out.println(response1.getStatusLine());
+			HttpEntity entity = response1.getEntity();
+			unparsedResponse = EntityUtils.toString(entity);
+			EntityUtils.consume(entity);
+		} finally {
+			response1.close();
+		}
+		return unparsedResponse;
+
+	}
+
+	public void setAuthentication(String host, int port, String username, String password) {
+		CredentialsProvider credsProvider = new BasicCredentialsProvider();
+		credsProvider.setCredentials(new AuthScope(host, port), new UsernamePasswordCredentials(username, password));
+		CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+	}
 
 	public static CoordinatesModel getLatAndLongFromAddress(String url) throws IOException {
 
