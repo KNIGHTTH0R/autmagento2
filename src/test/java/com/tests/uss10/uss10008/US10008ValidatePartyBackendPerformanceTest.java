0fb18bb18ef0398890aaf5db80a49925fc3224c1
ioanaurcan <ioanaurcan@EVO162.evointern>
2015-09-22 17:23:16 +0300

    work on Party backend bonus validation

--- src/test/java/com/tests/uss10/uss10008/US10008ValidatePartyBackendPerformanceTest.java
+++ src/test/java/com/tests/uss10/uss10008/US10008ValidatePartyBackendPerformanceTest.java
@@ -12,8 +12,10 @@ import org.junit.runner.RunWith;
 import com.steps.backend.BackEndSteps;
 import com.steps.backend.stylecoach.PartyListBackendSteps;
 import com.tests.BaseTest;
+import com.tools.data.UrlModel;
 import com.tools.data.backend.PartyBackendPerformanceModel;
 import com.tools.data.frontend.ClosedPartyPerformanceModel;
+import com.tools.env.variables.Credentials;
 import com.tools.persistance.MongoReader;
 import com.tools.requirements.Application;
 import com.workflows.backend.BackendPartyPerformanceValidationWorkflows;
@@ -31,17 +33,22 @@ public class US10008ValidatePartyBackendPerformanceTest extends BaseTest {
 	@Steps
 	public PartyListBackendSteps partyListBackendSteps;
 	ClosedPartyPerformanceModel expectedModel;
+	public static UrlModel urlModel = new UrlModel();
+	String partyId;
 
 	@Before
 	public void setUp() {
+		urlModel = MongoReader.grabUrlModels("US10008CreatePartyWithNewContactHostTest").get(0);
 		expectedModel = MongoReader.grabClosedPartyPerformanceModel("US10008ClosePartyAnfVerifyCommissionBonusesTest").get(0);
+		String[] parts = urlModel.getUrl().split("/");
+		partyId = parts[parts.length - 1];
 	}
 
 	@Test
 	public void us10008ValidatePartyBackendPerformanceTest() {
-		backEndSteps.performAdminLogin("", "");
+		backEndSteps.performAdminLogin(Credentials.BE_USER, Credentials.BE_PASS);
 		backEndSteps.clickOnStyleParties();
-		partyListBackendSteps.openPartyDetails("");
+		partyListBackendSteps.openPartyDetails(partyId);
 		PartyBackendPerformanceModel grabbedModel = partyListBackendSteps.getPartyBackendPerformance();
 		backendPartyPerformanceValidationWorkflows.validateClosedPartyPerformance(expectedModel, grabbedModel);
 	}
