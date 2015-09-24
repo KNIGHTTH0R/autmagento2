c5e2548637a6b4d5cda5ec59f699d53d290d6883
mihaibarta <mihai.barta@evozon.com>
2015-09-24 17:21:35 +0300

    minor fix

--- src/main/java/com/steps/frontend/DashboardSteps.java
+++ src/main/java/com/steps/frontend/DashboardSteps.java
@@ -56,13 +56,13 @@ public class DashboardSteps extends AbstractSteps {
 
 	@Step
 	public void validateDashboardTotalJewerlyBonus(String expectedTotal, String actualTotal) {
-		Assert.assertTrue("The dashboard total JB doesn't match - Expected: " + expectedTotal + "   Actual: " + actualTotal,
+		Assert.assertTrue("The dashboard total JB doesn't match - Expected: " + expectedTotal + "   Actual: " + actualTotal.replace(".", "").replace(",", "."),
 				expectedTotal.contains(actualTotal.replace(".", "").replace(",", ".")));
 	}
 
 	@Step
 	public void validateDashboardTotalMarketingBonus(String expectedTotal, String actualTotal) {
-		Assert.assertTrue("The dashboard totals MMB doesn't match - Expected: " + expectedTotal + "   Actual: " + actualTotal,
+		Assert.assertTrue("The dashboard totals MMB doesn't match - Expected: " + expectedTotal + "   Actual: " + actualTotal.replace(".", "").replace(",", "."),
 				expectedTotal.contains(actualTotal.replace(".", "").replace(",", ".")));
 	}
 
