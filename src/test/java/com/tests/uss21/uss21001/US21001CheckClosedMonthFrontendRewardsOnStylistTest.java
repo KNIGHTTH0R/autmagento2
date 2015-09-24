feca6553550b31be75e2ac4e99104729459123c5
mihaibarta <mihai.barta@evozon.com>
2015-09-24 17:07:43 +0300

    minor fix

--- src/test/java/com/tests/uss21/uss21001/US21001CheckClosedMonthFrontendRewardsOnStylistTest.java
+++ src/test/java/com/tests/uss21/uss21001/US21001CheckClosedMonthFrontendRewardsOnStylistTest.java
@@ -1,5 +1,11 @@
 package com.tests.uss21.uss21001;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import net.thucydides.core.annotations.Steps;
 import net.thucydides.core.annotations.Story;
 import net.thucydides.core.annotations.WithTag;
@@ -16,6 +22,8 @@ import com.steps.frontend.HeaderSteps;
 import com.steps.frontend.reports.JewelryBonusHistorySteps;
 import com.tests.BaseTest;
 import com.tools.data.backend.RewardPointsOfStylistModel;
+import com.tools.env.constants.FilePaths;
+import com.tools.env.variables.UrlConstants;
 import com.tools.persistance.MongoReader;
 import com.tools.requirements.Application;
 
@@ -41,6 +49,29 @@ public class US21001CheckClosedMonthFrontendRewardsOnStylistTest extends BaseTes
 
 	@Before
 	public void setUp() {
+		
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream(UrlConstants.RESOURCES_PATH + FilePaths.US_21_FOLDER + File.separator + "us21001.properties");
+			prop.load(input);
+			username = prop.getProperty("username");
+			password = prop.getProperty("password");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 
 		finalRewardPointsOfStylistModel = MongoReader.grabReviewPoints("US21001CheckStylistBonusesAfterClosedMonthTest").get(0);
 
@@ -54,6 +85,7 @@ public class US21001CheckClosedMonthFrontendRewardsOnStylistTest extends BaseTes
 		}
 		headerSteps.selectLanguage(MongoReader.getContext());
 		headerSteps.goToProfile();
+
 		String dashboardTotalJb = dashboardSteps.getJewelryBonus();
 		String dashboardTotalMmb = dashboardSteps.getMarketingMaterialBonus();
 
