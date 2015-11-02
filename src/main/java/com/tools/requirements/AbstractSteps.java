c128951f65d653adebf26be3018dd2068c8c811e
mihaibarta <mihai.barta@evozon.com>
2015-11-02 17:19:40 +0200

    minor fix

--- src/main/java/com/tools/requirements/AbstractSteps.java
+++ src/main/java/com/tools/requirements/AbstractSteps.java
@@ -240,7 +240,9 @@ public class AbstractSteps extends ScenarioSteps {
 
 	@Step
 	public void navigateAndAuthenticate(String URL) throws IOException, InterruptedException {
+		System.out.println(getDriver());
 		if (getDriver() == new FirefoxDriver()) {
+	
 			FirefoxProfile profile = new FirefoxProfile();
 			profile.setPreference("network.http.phishy-userpass-length", 255);
 			profile.setPreference("network.automatic-ntlm-auth.trusted-uris", "http://148.251.178.207");
