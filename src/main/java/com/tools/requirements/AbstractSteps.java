a373d8140193bedc0670c36bff7e16eae05aed4f
mihaibarta <mihai.barta@evozon.com>
2015-11-02 16:01:02 +0200

    minor fix

--- src/main/java/com/tools/requirements/AbstractSteps.java
+++ src/main/java/com/tools/requirements/AbstractSteps.java
@@ -233,9 +233,10 @@ public class AbstractSteps extends ScenarioSteps {
 	}
 
 	@Step
-	public void navigateAndAuthenticate(String URL) throws IOException {
+	public void navigateAndAuthenticate(String URL) throws IOException, InterruptedException {
 		getDriver().get(URL);
-		Runtime.getRuntime().exec("src/main/resources/FirefoxLogin.exe");
+		Runtime.getRuntime().exec("src\\main\\resources\\FirefoxLogin.exe");
+		Thread.sleep(5000);
 		
 //		WebDriverWait wait = new WebDriverWait(getDriver(), 120);
 //		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
