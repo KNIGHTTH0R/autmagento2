4882856e785d9ee3af0b9b55ae1f67545fb254ed
mihaibarta <mihai.barta@evozon.com>
2015-11-06 13:24:25 +0200

    minor fix

--- src/main/java/com/workflows/frontend/AdyenWorkflows.java
+++ src/main/java/com/workflows/frontend/AdyenWorkflows.java
@@ -1,10 +1,15 @@
 package com.workflows.frontend;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
 import net.thucydides.core.annotations.Step;
 
 import com.tools.CustomVerification;
 import com.tools.data.backend.OrderModel;
 import com.tools.data.frontend.ShippingModel;
+import com.tools.env.constants.ComissionConfigConstants;
 
 public class AdyenWorkflows {
 
@@ -23,8 +28,12 @@ public class AdyenWorkflows {
 	}
 
 	@Step
-	public void verifyTotal(String productNow, String compare) {
-		CustomVerification.verifyTrue("Failure: Adyen totals doesn't match   Expected: " + compare + " Actual: " + productNow, productNow.contentEquals(compare.replace(".", "")));
+	public void verifyTotal(String adyenTotal, String compare) {
+		BigDecimal total = BigDecimal.valueOf(Double.parseDouble(adyenTotal));
+		MathContext mc = new MathContext(2, RoundingMode.HALF_UP);
+		total = total.divide(BigDecimal.valueOf(100), mc);
+		CustomVerification.verifyTrue("Failure: Adyen totals doesn't match   Expected: " + compare + " Actual: " + String.valueOf(total),
+				adyenTotal.contains(String.valueOf(total)));
 	}
 
 }
