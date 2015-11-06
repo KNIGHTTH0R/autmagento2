878b3775000af2d14c515f9677ce2521ce3ff161
mihaibarta <mihai.barta@evozon.com>
2015-11-06 13:51:07 +0200

    minor fix

--- src/main/java/com/workflows/frontend/AdyenWorkflows.java
+++ src/main/java/com/workflows/frontend/AdyenWorkflows.java
@@ -1,7 +1,6 @@
 package com.workflows.frontend;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 
 import net.thucydides.core.annotations.Step;
@@ -9,7 +8,6 @@ import net.thucydides.core.annotations.Step;
 import com.tools.CustomVerification;
 import com.tools.data.backend.OrderModel;
 import com.tools.data.frontend.ShippingModel;
-import com.tools.env.constants.ComissionConfigConstants;
 
 public class AdyenWorkflows {
 
@@ -24,15 +22,13 @@ public class AdyenWorkflows {
 	@Step
 	public void veryfyAdyenTotals(String string) {
 
-		verifyTotal(orderModel.getTotalPrice(), shippingCalculatedModel.getSubTotal());
+		verifyTotal(orderModel.getTotalPrice(), shippingCalculatedModel.getTotalAmount());
 	}
 
 	@Step
 	public void verifyTotal(String adyenTotal, String compare) {
 		BigDecimal total = BigDecimal.valueOf(Double.parseDouble(adyenTotal));
 		System.out.println(total);
-		MathContext mc = new MathContext(2, RoundingMode.HALF_UP);
-//		total = total.divide(BigDecimal.valueOf(100), mc);
 		total = total.divide(BigDecimal.valueOf(100), RoundingMode.HALF_UP);
 		System.out.println(total);
 		CustomVerification.verifyTrue("Failure: Adyen totals doesn't match   Expected: " + compare + " Actual: " + String.valueOf(total),
