43bc2b27e80754442b6579e8ae0d744e44c1395b
mihaibarta <mihai.barta@evozon.com>
2015-11-11 19:04:12 +0200

    minor fix

--- src/test/java/com/tests/uss23/US23001VerifyStockSyncAfterOrderImportTest.java
+++ src/test/java/com/tests/uss23/US23001VerifyStockSyncAfterOrderImportTest.java
@@ -63,8 +63,8 @@ public class US23001VerifyStockSyncAfterOrderImportTest extends BaseTest {
 
 		orderModel = MongoReader.getOrderModel("US23001BuyProductsOnShopforMyselfTest").get(0);
 		OrderStatusModel orderStatusModel = NavQueries.getProductSyncronizedStatus(orderModel.getOrderId().toUpperCase());
-		String[] parts = orderStatusModel.getSyncDate().split(":");
-		String syncDate = parts[0] + parts[1] + parts[2];
+		String[] parts = orderStatusModel.getSyncDate().split(".");
+		String syncDate = parts[0];
 
 		initialChangingMagentoProducts = MongoReader.grabStockInfoModel("US23001GetMagAndNavStockBerforeOrderTest" + SoapKeys.MAGENTO_INITIAL_CHANGING_STOCK);
 		initialChangingNavProducts = MongoReader.grabStockInfoModel("US23001GetMagAndNavStockBerforeOrderTest" + SoapKeys.NAVISION_INITIAL_CHANGING_STOCK);
