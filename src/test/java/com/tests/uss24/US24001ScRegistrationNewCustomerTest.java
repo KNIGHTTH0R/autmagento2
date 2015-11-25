af8df75853201f1c987fc670b5bb91ba13a43287
mihaibarta <mihai.barta@evozon.com>
2015-11-25 13:56:49 +0200

    minor fix

--- src/test/java/com/tests/uss24/US24001ScRegistrationNewCustomerTest.java
+++ src/test/java/com/tests/uss24/US24001ScRegistrationNewCustomerTest.java
@@ -26,7 +26,7 @@ import com.tools.requirements.Application;
 @WithTag(name = "US6.1 Sc Registration New Customer Test ", type = "Scenarios")
 @Story(Application.StylecoachRegistration.US6_1.class)
 @RunWith(ThucydidesParameterizedRunner.class)
-@UseTestDataFrom("\\resources\\csv\\validPlzTestData.csv")
+@UseTestDataFrom("/resources/validPlzTestData.csv")
 public class US24001ScRegistrationNewCustomerTest extends BaseTest {
 
 	@Steps
