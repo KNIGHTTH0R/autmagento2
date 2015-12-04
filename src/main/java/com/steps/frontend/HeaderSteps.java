a4639eb4b84fe19339cb16520c458097392ca525
mihaibarta <mihai.barta@evozon.com>
2015-12-04 19:16:35 +0200

    minor fixes

--- src/main/java/com/steps/frontend/HeaderSteps.java
+++ src/main/java/com/steps/frontend/HeaderSteps.java
@@ -181,7 +181,6 @@ public class HeaderSteps extends AbstractSteps {
 		partyCreationPage().clickOrderForHostess();
 	}
 
-	@Step
 	public boolean succesfullLogin() {
 		return headerPage().succesfullLogin();
 	}
