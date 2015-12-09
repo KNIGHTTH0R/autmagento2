8e1b4e2bac65940c4cbb34a153c8eb8b3633156c
mihaibarta <mihai.barta@evozon.com>
2015-12-09 14:23:51 +0200

    WORKED ON SC REG SHIPPING COSTS VALDIATION

--- src/test/java/com/tests/us6/us6001b/US6001bScRegistrationNewCustForbiddenCountryTest.java
+++ src/test/java/com/tests/us6/us6001b/US6001bScRegistrationNewCustForbiddenCountryTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.connectors.mongo.MongoConnector;
+import com.pages.frontend.checkout.cart.stylistRegistration.StylistRegistrationCartTotalModel;
 import com.steps.frontend.HeaderSteps;
 import com.steps.frontend.StylistCampaignSteps;
 import com.steps.frontend.StylistRegistrationSteps;
@@ -19,9 +20,11 @@ import com.tools.CustomVerification;
 import com.tools.data.frontend.AddressModel;
 import com.tools.data.frontend.CustomerFormModel;
 import com.tools.data.frontend.DateModel;
+import com.tools.datahandlers.stylistRegistration.StylistRegDataGrabber;
 import com.tools.env.variables.ContextConstants;
 import com.tools.persistance.MongoWriter;
 import com.tools.requirements.Application;
+import com.workflows.frontend.stylecoachRegistration.StylecoachRegistrationCartWorkflows;
 
 @WithTag(name = "US6.1b Sc Registration New Customer Forbidden Country Test ", type = "Scenarios")
 @Story(Application.StylecoachRegistration.US6_1.class)
@@ -36,11 +39,14 @@ public class US6001bScRegistrationNewCustForbiddenCountryTest extends BaseTest {
 	public StylistRegistrationSteps stylistRegistrationSteps;
 	@Steps
 	public CustomVerification customVerification;
+	@Steps
+	public StylecoachRegistrationCartWorkflows stylecoachRegistrationCartWorkflows;
 
 	private CustomerFormModel customerFormData;
 	private DateModel customerFormDate = new DateModel();
 	private DateModel birthDate = new DateModel();
 	private AddressModel customerFormAddress;
+	private StylistRegistrationCartTotalModel calculatedTotals;
 
 	@Before
 	public void setUp() throws Exception {
@@ -48,6 +54,9 @@ public class US6001bScRegistrationNewCustForbiddenCountryTest extends BaseTest {
 		customerFormData = new CustomerFormModel();
 		customerFormAddress = new AddressModel();
 		customerFormAddress.setCountryName(ContextConstants.NOT_PREFERED_LANGUAGE);
+		calculatedTotals = new StylistRegistrationCartTotalModel();
+		calculatedTotals.setDelivery("0.00");
+		calculatedTotals.setTotalPrice("100.00");
 		birthDate.setDate("Feb,1970,12");
 		MongoConnector.cleanCollection(getClass().getSimpleName());
 	}
@@ -57,6 +66,10 @@ public class US6001bScRegistrationNewCustForbiddenCountryTest extends BaseTest {
 		headerSteps.navigateToRegisterForm();
 		String formCreationDate = stylistRegistrationSteps.fillCreateCustomerFormFirstWithForbiddenCountry(customerFormData, customerFormAddress, birthDate.getDate());
 		customerFormDate.setDate(formCreationDate);
+		
+		stylecoachRegistrationCartWorkflows.setVerifyTotalsDiscount(calculatedTotals, StylistRegDataGrabber.cartTotals);
+		stylecoachRegistrationCartWorkflows.verifyTotalsDiscount();
+		
 		customVerification.printErrors();
 		
 	}
