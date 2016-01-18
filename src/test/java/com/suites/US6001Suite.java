31991330ddd51d108148766b0fae1a392e306752
mihaibarta <mihai.barta@evozon.com>
2016-01-18 17:48:46 +0200

    minor fix

--- src/test/java/com/suites/US6001Suite.java
+++ src/test/java/com/suites/US6001Suite.java
@@ -5,6 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import com.tests.us6.us6001.US6001CancelStarterSetOrderTest;
+import com.tests.us6.us6001.US6001CheckAssociatedContactCreationTest;
 import com.tests.us6.us6001.US6001CheckStylistActivationStarterkitCanceledTest;
 import com.tests.us6.us6001.US6001CheckStylistActivationTest;
 import com.tests.us6.us6001.US6001CheckStylistPreferedWebsiteAndLanguage;
@@ -17,6 +18,7 @@ import com.tests.us6.us6001.US6001ScRegistrationNewCustomerTest;
 	US6001CheckStylistPreferedWebsiteAndLanguage.class,
 	US6001CancelStarterSetOrderTest.class,
 	US6001CheckStylistActivationStarterkitCanceledTest.class,
+	US6001CheckAssociatedContactCreationTest.class,
 })
 @RunWith(Suite.class)
 public class US6001Suite {
