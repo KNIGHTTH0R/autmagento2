5565b7b82f933efee14eb3bad77387ec7b38bf15
mihaibarta <mihai.barta@evozon.com>
2016-01-25 16:54:49 +0200

    minor fix

--- src/test/java/com/tests/us6/us6001b/US6001bCheckStylistPreferedWebsiteAndLanguage.java
+++ src/test/java/com/tests/us6/us6001b/US6001bCheckStylistPreferedWebsiteAndLanguage.java
@@ -12,6 +12,7 @@ import org.junit.runner.RunWith;
 import com.steps.frontend.CustomerRegistrationSteps;
 import com.tests.BaseTest;
 import com.tools.data.frontend.CustomerFormModel;
+import com.tools.env.variables.ContextConstants;
 import com.tools.persistance.MongoReader;
 import com.tools.requirements.Application;
 
@@ -37,7 +38,7 @@ public class US6001bCheckStylistPreferedWebsiteAndLanguage extends BaseTest {
 	@Test
 	public void us6001bCheckStylistPreferedWebsiteAndLanguage() {
 
-		customerRegistrationSteps.performLoginAndVerifyWebsiteAndLanguage(stylistRegistrationData.getEmailName(), stylistRegistrationData.getPassword(),MongoReader.getContext(),MongoReader.getContext());
+		customerRegistrationSteps.performLoginAndVerifyWebsiteAndLanguage(stylistRegistrationData.getEmailName(), stylistRegistrationData.getPassword(),MongoReader.getContext(),ContextConstants.NOT_PREFERED_WEBSITE);
 
 	}
 }
