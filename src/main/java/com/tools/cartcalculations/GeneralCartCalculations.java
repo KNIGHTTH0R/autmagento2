3117724b59e3c566859b4885a962170b13f3c255
mihaibarta <mihai.barta@evozon.com>
2016-02-02 16:12:30 +0200

    refactoring

--- src/main/java/com/tools/cartcalculations/GeneralCartCalculations.java
+++ src/main/java/com/tools/cartcalculations/GeneralCartCalculations.java
@@ -7,7 +7,7 @@ public class GeneralCartCalculations {
 	public static String calculateNewShippingBasedOnRemaingSumFromRuleDiscount(BigDecimal totalAmount, BigDecimal ruleDiscount, BigDecimal shipping) {
 
 		if (ruleDiscount.compareTo(totalAmount) > 0) {
-
+			
 			BigDecimal remainingSum = ruleDiscount.subtract(totalAmount);
 			shipping = shipping.subtract(remainingSum);
 			shipping = shipping.compareTo(BigDecimal.ZERO) > 0 ? shipping : BigDecimal.ZERO;
