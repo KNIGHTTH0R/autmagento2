03071994b5e9de1ecb0c7160b2b8b77484268b02
mihaibarta <mihai.barta@evozon.com>
2016-02-02 15:08:24 +0200

    refactoring on cart calculations

--- src/main/java/com/tools/cartcalculations/GeneralCartCalculations.java
+++ src/main/java/com/tools/cartcalculations/GeneralCartCalculations.java
@@ -6,17 +6,14 @@ public class GeneralCartCalculations {
 
 	public static String calculateNewShippingBasedOnRemaingSumFromRuleDiscount(BigDecimal totalAmount, BigDecimal ruleDiscount, BigDecimal shipping) {
 
-		BigDecimal newShipping = BigDecimal.ZERO;
-
 		if (ruleDiscount.compareTo(totalAmount) > 0) {
+
 			BigDecimal remainingSum = ruleDiscount.subtract(totalAmount);
-			newShipping = shipping.subtract(remainingSum);
-			newShipping = newShipping.compareTo(BigDecimal.ZERO) > 0 ? newShipping : BigDecimal.ZERO;
-		} else {
-			newShipping = shipping;
+			shipping = shipping.subtract(remainingSum);
+			shipping = shipping.compareTo(BigDecimal.ZERO) > 0 ? shipping : BigDecimal.ZERO;
 		}
 
-		return String.valueOf(newShipping);
+		return String.valueOf(shipping);
 	}
 
 }
