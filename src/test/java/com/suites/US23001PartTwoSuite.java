4c069f55c4695f33523d3d3eab9f0242f1b5b376
mihaibarta <mihai.barta@evozon.com>
2016-03-01 18:14:14 +0200

    minor fix

--- src/test/java/com/suites/US23001PartTwoSuite.java
+++ src/test/java/com/suites/US23001PartTwoSuite.java
@@ -4,11 +4,19 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.tests.uss23.US23001GetMagAndNavStockBerforeTpOrderTest;
+import com.tests.uss23.US23001PayAndImportTermPurchaseOrderTest;
+import com.tests.uss23.US23001PlaceTermPurchaseOrderTest;
+import com.tests.uss23.US23001VerifyMagAndNavStockAfterTPOrderTest;
 import com.tests.uss23.US23001VerifyStockSyncAfterOrderImportTest;
 
 
 @SuiteClasses({
 	US23001VerifyStockSyncAfterOrderImportTest.class,
+	US23001GetMagAndNavStockBerforeTpOrderTest.class,
+	US23001PlaceTermPurchaseOrderTest.class,
+	US23001PayAndImportTermPurchaseOrderTest.class,
+	US23001VerifyMagAndNavStockAfterTPOrderTest.class,
 })
 @RunWith(Suite.class)
 public class US23001PartTwoSuite {
