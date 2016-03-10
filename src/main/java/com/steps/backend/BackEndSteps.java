71a68a74604baec58ff5605ad755cbfbfcf7c717
mihaibarta <mihai.barta@evozon.com>
2016-03-10 14:37:31 +0200

    minor fix

--- src/main/java/com/steps/backend/BackEndSteps.java
+++ src/main/java/com/steps/backend/BackEndSteps.java
@@ -25,7 +25,7 @@ public class BackEndSteps extends AbstractSteps {
 		magentoLoginPage().inputUserName(userName);
 		magentoLoginPage().inputUserPassword(userPass);
 		magentoLoginPage().clickOnLogin();
-		dismissPopUp();
+//		dismissPopUp();
 	}
 
 	@Step
