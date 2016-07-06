de54b1587cf20fa4b809bd2ae7dc4681d6b7e9b9
Barta Mihai <mihai.barta@evozon.com>
2016-07-06 19:52:00 +0300

    added some waits to order success page

--- src/main/java/com/pages/frontend/checkout/SuccessPage.java
+++ src/main/java/com/pages/frontend/checkout/SuccessPage.java
@@ -1,11 +1,12 @@
 package com.pages.frontend.checkout;
 
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.tools.env.constants.ContextConstants;
 import com.tools.requirements.AbstractPage;
@@ -19,12 +20,14 @@ public class SuccessPage extends AbstractPage {
 	private WebElement messageContainer;
 
 	public void verifySuccessMessage() {
-		
-//		Wait<WebDriver> wait = new WebDriverWait(getDriver(), 30);
-//		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".blockUI.blockMsg.blockElement")));
-		withTimeoutOf(5, TimeUnit.SECONDS).waitFor(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".blockUI.blockMsg.blockElement")));
+
+		Wait<WebDriver> wait = new WebDriverWait(getDriver(), 30);
+		wait.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".blockUI.blockMsg.blockElement")));
+		// withTimeoutOf(5,
+		// TimeUnit.SECONDS).waitFor(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(".blockUI.blockMsg.blockElement")));
 		element(messageContainer).waitUntilVisible();
-		Assert.assertTrue("Failure: Success message has not been found.", messageContainer.getText().contains(ContextConstants.SUCCES_MESSAGE));
+		Assert.assertTrue("Failure: Success message has not been found.",
+				messageContainer.getText().contains(ContextConstants.SUCCES_MESSAGE));
 	}
 
 }
