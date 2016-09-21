c4c9ee2e4b8ecc0f6eddcde864a8f0d7a2f8ff6b
Barta Mihai <mihai.barta@evozon.com>
2016-09-21 17:51:51 +0300

    worked on team report

---
+++ src/main/java/com/tools/generalCalculation/CommissionService.java
@@ -0,0 +1,27 @@
+package com.tools.generalCalculation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import com.connectors.http.ComissionRestCalls;
+import com.tools.data.commission.CommissionStylistModel;
+
+public class CommissionService {
+
+	// TODO move this from here
+	public static List<CommissionStylistModel> getStylistsFromLevel(List<CommissionStylistModel> allStylists, String stylistId, int level) throws Exception {
+
+		List<CommissionStylistModel> levelStylists = new ArrayList<CommissionStylistModel>();
+
+		for (CommissionStylistModel stylist : allStylists) {
+			String[] ancestors = stylist.getAncestors().split(",");
+			if (ArrayUtils.indexOf(ancestors, stylistId) == level - 1) {
+				levelStylists.add(stylist);
+			}
+		}
+		return levelStylists;
+	}
+
+}
