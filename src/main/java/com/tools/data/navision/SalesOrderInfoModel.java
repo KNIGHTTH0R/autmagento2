4fed67feb365c2d78adfe6760b627a7f410c5661
Emilian Melian <emilian.melian@evozon.com>
2016-11-29 13:48:41 +0200

    created: OrderInfoMagCalls method created: SalesOrderInfoModel updated: DBOrderModel

---
+++ src/main/java/com/tools/data/navision/SalesOrderInfoModel.java
@@ -0,0 +1,23 @@
+package com.tools.data.navision;
+
+public class SalesOrderInfoModel {
+	private String item_id;
+	private String sku;
+
+	public String getItem_id() {
+		return item_id;
+	}
+
+	public void setItem_id(String item_id) {
+		this.item_id = item_id;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+}
