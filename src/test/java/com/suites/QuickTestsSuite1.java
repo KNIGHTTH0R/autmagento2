1c08cb53f1910478d78b2da66c4c0d06608af63f
Emilian Melian <emilian.melian@evozon.com>
2017-03-23 12:12:47 +0200

    update on quick test suite

--- src/test/java/com/suites/QuickTestsSuite1.java
+++ src/test/java/com/suites/QuickTestsSuite1.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.tests.us6.us6001.US6001ScRegistrationNewCustomerTest;
+
 @SuiteClasses({
-	US6001Suite.class,
+	US6001ScRegistrationNewCustomerTest.class,
 })
 @RunWith(Suite.class)
 public class QuickTestsSuite1 {
--- src/test/java/com/suites/QuickTestsSuite3.java
+++ src/test/java/com/suites/QuickTestsSuite3.java
@@ -4,8 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.tests.us3.us3001.US3001BuyProductsForTheFirstTimeTest;
+
 @SuiteClasses({
-	US3001Suite.class,
+	US3001BuyProductsForTheFirstTimeTest.class,
 })
 @RunWith(Suite.class)
 public class QuickTestsSuite3 {
