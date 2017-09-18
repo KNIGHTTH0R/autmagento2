bcfbbd7ff6ceff0bddfe678cc1ecfe0d30f19a2f
Emilian Melian <emilian.melian@evozon.com>
2017-09-18 13:34:49 +0300

    chromedriver update

--- src/test/java/com/tests/SafariUsage.java
+++ src/test/java/com/tests/SafariUsage.java
@@ -1,10 +1,15 @@
 package com.tests;
 
+import java.net.URL;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -13,15 +18,28 @@ import net.serenitybdd.core.annotations.findby.By;
 
 public class SafariUsage {
 	 
-	public static void main(String[] args)
-	{
-		WebDriver driver = new SafariDriver();
+	@Test
+	public void safariUsage() throws Exception {
+		RemoteWebDriver driver = new RemoteWebDriver(
+                new URL("http://localhost:4444/wd/hub"),
+                DesiredCapabilities.chrome());
 		driver.get("http://store.demoqa.com");
  
 		//Find some element on DemoQa.com
 		WebElement element = driver.findElement(By.id("login"));
 		element.click();
- 
 	}
+//	
+//	public static void main(String[] args)
+//	{
+//	//	WebDriver driver = new SafariDriver();
+//		WebDriver driver = new ChromeDriver();
+//		driver.get("http://store.demoqa.com");
+// 
+//		//Find some element on DemoQa.com
+//		WebElement element = driver.findElement(By.id("login"));
+//		element.click();
+// 
+//	}
  
 }
