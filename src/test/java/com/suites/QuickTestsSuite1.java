a2b4953c1bc84353a4505f62d782cb862262ce0a
Emilian Melian <emilian.melian@evozon.com>
2017-09-26 17:28:00 +0300

    update pom (serenity version )

--- src/test/java/com/suites/QuickTestsSuite1.java
+++ src/test/java/com/suites/QuickTestsSuite1.java
@@ -7,10 +7,38 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.tests.us7.us7001.US7001ConfirmCustomerTest;
 import com.tests.us7.us7001.US7001RegularCustRegOnMasterTest;
 
-@SuiteClasses({
 
+@SuiteClasses({
+//	CreateProductsTest.class,
+
+	//shop for myself suite
+	//US3001Suite.class,
+//	US3003Suite.class,
+//	US3004Suite.class,
+//	US3006Suite.class,
+//	US3007Suite.class,
+//	US3009Suite.class,
+//	US30010Suite.class,
+//	US30011Suite.class,
+//	US30012SfmSpecialPriceProductTest.class,
+//	
+//	US4001Suite.class,
+//	US4002Suite.class,
+//	
+//	
 	US7001RegularCustRegOnMasterTest.class,
 	US7001ConfirmCustomerTest.class,
+//	US7001bSuite.class,
+////	US7002Suite.class,
+////	US7004Suite.class,
+////	//US7004bSuite.class,
+//	
+//	
+////	US7008Suite.class,
+//	US7009Suite.class,
+//	US70010Suite.class,
+//	US70011Suite.class,
+//	US70012Suite.class,
 	
 })
 @RunWith(Suite.class)
