191f760b90161b997f153e12b863ea5f0f22e904
Emilian Melian <emilian.melian@evozon.com>
2017-09-28 15:49:59 +0300

    zalenium suite 3

--- src/test/java/com/suites/ZaleniumSuite3.java
+++ src/test/java/com/suites/ZaleniumSuite3.java
@@ -4,15 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.tests.CreateProductsTest;
-import com.tests.uss10.us10001.US10001CreatePartyWithCustomerHostTest;
-import com.tests.uss10.us10001b.US10001bCreatePartyWithStylistHostTest;
-import com.tests.uss10.us10002b.US10002bCreatePartyWithCustomerHostTest;
+import com.tests.us6.us6001.US6001ScRegistrationNewCustomerTest;
 
 @SuiteClasses({
-	CreateProductsTest.class,
-	//shop for myself suite
-	US3001Suite.class,
+	US6001ScRegistrationNewCustomerTest.class,
 	 
 })
 @RunWith(Suite.class)
