e73e64446d9a4b5cfd4b771904ae12727da4703b
Emilian Melian <emilian.melian@evozon.com>
2017-09-28 13:53:37 +0300

    update on quick suite 1

--- src/test/java/com/suites/QuickTestsSuite1.java
+++ src/test/java/com/suites/QuickTestsSuite1.java
@@ -4,30 +4,26 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.tests.us3.us30012.US30012SfmSpecialPriceProductTest;
 import com.tests.us7.us7001.US7001ConfirmCustomerTest;
 import com.tests.us7.us7001.US7001RegularCustRegOnMasterTest;
 
 
 @SuiteClasses({
-//	CreateProductsTest.class,
-
+	US10CreateAllPartiesSuite.class,
 	//shop for myself suite
-	//US3001Suite.class,
-//	US3003Suite.class,
-//	US3004Suite.class,
-//	US3006Suite.class,
-//	US3007Suite.class,
-//	US3009Suite.class,
-//	US30010Suite.class,
-//	US30011Suite.class,
-//	US30012SfmSpecialPriceProductTest.class,
+	US3001Suite.class,
+	US3003Suite.class,
+	US3004Suite.class,
+	US3006Suite.class,
+	US3007Suite.class,
+	US3009Suite.class,
+	US30010Suite.class,
+	US30011Suite.class,
+	US30012SfmSpecialPriceProductTest.class,
 //	
-//	US4001Suite.class,
-//	US4002Suite.class,
-//	
-//	
-	US7001RegularCustRegOnMasterTest.class,
-	US7001ConfirmCustomerTest.class,
+//	US7001RegularCustRegOnMasterTest.class,
+//	US7001ConfirmCustomerTest.class,
 //	US7001bSuite.class,
 ////	US7002Suite.class,
 ////	US7004Suite.class,
